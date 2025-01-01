36ace62b80d37a604658689dfbbb13eacadcc1cb
hofovea <m.o.zashchyk@gmail.com>
2025-01-01 15:34:23 +0200

    added windows installers

--- lib/core/notifications/notifications.dart
+++ lib/core/notifications/notifications.dart
@@ -1,22 +1,34 @@
-import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'dart:io';
 
-FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:windows_notification/notification_message.dart';
+import 'package:windows_notification/windows_notification.dart';
+
+FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+    FlutterLocalNotificationsPlugin();
+
+final winNotifyPlugin = WindowsNotification(
+  applicationId:
+      r"{9aaabc6b-ee42-4274-9c6e-a96c05cad646\WindowsPowerShell\v1.0\powershell.exe",
+);
 
 Future<void> setupNotificationsPlugin() async {
-  const initializationSettingsDarwin = DarwinInitializationSettings();
-  const initializationSettingsLinux = LinuxInitializationSettings(
-    defaultActionName: 'Open notification',
-  );
-  const initializationSettings = InitializationSettings(
-    macOS: initializationSettingsDarwin,
-    linux: initializationSettingsLinux,
-  );
-  await flutterLocalNotificationsPlugin.initialize(
-    initializationSettings,
-    onDidReceiveNotificationResponse: (NotificationResponse response) {
-      print('Notification clicked with payload: ${response.payload}');
-    },
-  );
+  if (!Platform.isWindows) {
+    const initializationSettingsDarwin = DarwinInitializationSettings();
+    const initializationSettingsLinux = LinuxInitializationSettings(
+      defaultActionName: 'Open notification',
+    );
+    const initializationSettings = InitializationSettings(
+      macOS: initializationSettingsDarwin,
+      linux: initializationSettingsLinux,
+    );
+    await flutterLocalNotificationsPlugin.initialize(
+      initializationSettings,
+      onDidReceiveNotificationResponse: (NotificationResponse response) {
+        print('Notification clicked with payload: ${response.payload}');
+      },
+    );
+  }
 }
 
 Future<void> onDidReceiveLocalNotification(
--- lib/feats/notification_demo_screen/notification_demo_screen.dart
+++ lib/feats/notification_demo_screen/notification_demo_screen.dart
@@ -3,6 +3,7 @@ import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:keeper/core/core.dart';
+import 'package:windows_notification/notification_message.dart';
 
 class NotificationDemoScreen extends StatelessWidget {
   const NotificationDemoScreen({super.key});
@@ -13,21 +14,31 @@ class NotificationDemoScreen extends StatelessWidget {
       children: [
         ElevatedButton(
           onPressed: () async {
-            const macOSNotificationDetails =
-                DarwinNotificationDetails(subtitle: 'Here is some text');
-            const linuxOSNotificationDetails = LinuxNotificationDetails();
-            const notificationDetails = NotificationDetails(
-              macOS: macOSNotificationDetails,
-              linux: linuxOSNotificationDetails,
-            );
-            await flutterLocalNotificationsPlugin.show(
-              0,
-              'Test Notification',
-              Platform.isLinux
-                  ? 'This is a test notification on Linux.'
-                  : 'This is a test notification on macOS.',
-              notificationDetails,
-            );
+            if (Platform.isWindows) {
+              NotificationMessage message =
+                  NotificationMessage.fromPluginTemplate(
+                "id1",
+                "TITLE ON WINDOWS",
+                "Some useful content",
+              );
+              winNotifyPlugin.showNotificationPluginTemplate(message);
+            } else {
+              const macOSNotificationDetails =
+                  DarwinNotificationDetails(subtitle: 'Here is some text');
+              const linuxOSNotificationDetails = LinuxNotificationDetails();
+              const notificationDetails = NotificationDetails(
+                macOS: macOSNotificationDetails,
+                linux: linuxOSNotificationDetails,
+              );
+              await flutterLocalNotificationsPlugin.show(
+                0,
+                'Test Notification',
+                Platform.isLinux
+                    ? 'This is a test notification on Linux.'
+                    : 'This is a test notification on macOS.',
+                notificationDetails,
+              );
+            }
           },
           child: const Text('Send local notification'),
         ),
